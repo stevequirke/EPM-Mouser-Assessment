97e077544ed1f74a90f225dc768ddd04fd6e45fd
andyglassepm <113031470+andyglassepm@users.noreply.github.com>
2022-11-30 12:02:43 +0000

    Upload from old repo

---
+++ EPM.Mouser.Interview.Web/Controllers/WarehouseApi.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EPM.Mouser.Interview.Web.Controllers
+{
+    public class WarehouseApi : Controller
+    {
+
+        /*
+         *  Action: GET
+         *  Url: api/warehouse/id
+         *  This action should return a single product for an Id
+         */
+        public JsonResult GetProduct(long id)
+        {
+            return Json(null);
+        }
+
+        /*
+         *  Action: GET
+         *  Url: api/warehouse
+         *  This action should return a collection of products in stock
+         *  In stock means In Stock Quantity is greater than zero and In Stock Quantity is greater than the Reserved Quantity
+         */
+        [HttpGet]
+        public JsonResult GetPublicInStockProducts()
+        {
+            return Json(null);
+        }
+
+
+        /*
+         *  Action: GET
+         *  Url: api/warehouse/order
+         *  This action should return a EPM.Mouser.Interview.Models.UpdateResponse
+         *  This action should have handle an input parameter of EPM.Mouser.Interview.Models.UpdateQuantityRequest in JSON format in the body of the request
+         *       {
+         *           "id": 1,
+         *           "quantity": 1
+         *       }
+         *
+         *  This action should increase the Reserved Quantity for the product requested by the amount requested
+         *
+         *  This action should return failure (success = false) when:
+         *     - ErrorReason.NotEnoughQuantity when: The quantity being requested would increase the Reserved Quantity to be greater than the In Stock Quantity.
+         *     - ErrorReason.QuantityInvalid when: A negative number was requested
+         *     - ErrorReason.InvalidRequest when: A product for the id does not exist
+        */
+        public JsonResult OrderItem()
+        {
+            return Json(null);
+        }
+
+        /*
+         *  Url: api/warehouse/ship
+         *  This action should return a EPM.Mouser.Interview.Models.UpdateResponse
+         *  This action should have handle an input parameter of EPM.Mouser.Interview.Models.UpdateQuantityRequest in JSON format in the body of the request
+         *       {
+         *           "id": 1,
+         *           "quantity": 1
+         *       }
+         *
+         *
+         *  This action should:
+         *     - decrease the Reserved Quantity for the product requested by the amount requested to a minimum of zero.
+         *     - decrease the In Stock Quantity for the product requested by the amount requested
+         *
+         *  This action should return failure (success = false) when:
+         *     - ErrorReason.NotEnoughQuantity when: The quantity being requested would cause the In Stock Quantity to go below zero.
+         *     - ErrorReason.QuantityInvalid when: A negative number was requested
+         *     - ErrorReason.InvalidRequest when: A product for the id does not exist
+        */
+        public JsonResult ShipItem()
+        {
+            return Json(null);
+        }
+
+        /*
+        *  Url: api/warehouse/restock
+        *  This action should return a EPM.Mouser.Interview.Models.UpdateResponse
+        *  This action should have handle an input parameter of EPM.Mouser.Interview.Models.UpdateQuantityRequest in JSON format in the body of the request
+        *       {
+        *           "id": 1,
+        *           "quantity": 1
+        *       }
+        *
+        *
+        *  This action should:
+        *     - increase the In Stock Quantity for the product requested by the amount requested
+        *
+        *  This action should return failure (success = false) when:
+        *     - ErrorReason.QuantityInvalid when: A negative number was requested
+        *     - ErrorReason.InvalidRequest when: A product for the id does not exist
+        */
+        public JsonResult RestockItem()
+        {
+            return Json(null);
+        }
+
+        /*
+        *  Url: api/warehouse/add
+        *  This action should return a EPM.Mouser.Interview.Models.CreateResponse<EPM.Mouser.Interview.Models.Product>
+        *  This action should have handle an input parameter of EPM.Mouser.Interview.Models.Product in JSON format in the body of the request
+        *       {
+        *           "id": 1,
+        *           "inStockQuantity": 1,
+        *           "reservedQuantity": 1,
+        *           "name": "product name"
+        *       }
+        *
+        *
+        *  This action should:
+        *     - create a new product with:
+        *          - The requested name - But forced to be unique - see below
+        *          - The requested In Stock Quantity
+        *          - The Reserved Quantity should be zero
+        *
+        *       UNIQUE Name requirements
+        *          - No two products can have the same name
+        *          - Names should have no leading or trailing whitespace before checking for uniqueness
+        *          - If a new name is not unique then append "(1)" to the name [like windows file system does]
+        *
+        *
+        *  This action should return failure (success = false) and an empty Model property when:
+        *     - ErrorReason.QuantityInvalid when: A negative number was requested for the In Stock Quantity
+        *     - ErrorReason.InvalidRequest when: A blank or empty name is requested
+        */
+        public JsonResult AddNewProduct()
+        {
+            return Json(null);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,137 @@
+# Mouser/EPM Tech assessment
+
+The solution is written in VS2022 and .Net6
+
+There are two controllers that need completing
+ - WarehouseApi
+    - There comments that document the required actions/endpoints
+ - HomeController
+    - Check the index view for the controller to see whats requested 
+
+A DLL has been provided (EPM.Mouser.Interview.Data) which provides a data repo and some models
+The data layer is 'faked' and initialized on startup so don't expect the same data between debug sessions (but it is persisted between requests).
+
+
+Document any questions you'd ask and then also put your assumptions (in leau of asking the questions)
+Feel free to also suggest what else could be done out side the scope of the requested changes.
+Also document any enhacements you would apply.
+
+
+
+## Definition for EPM.Mouser.Interview.Data.IWarehouseRepository
+```
+public interface IWarehouseRepository
+{
+    /// <summary>
+    /// Gets the order for the given id
+    /// </summary>
+    /// <param name="id">The id of the product.</param>
+    Task<Product?> Get(long id);
+
+    /// <summary>
+    /// Lists all products.
+    /// </summary>
+    Task<List<Product>> List();
+
+    /// <summary>
+    /// Queries the specified product data and returns a matching list.
+    /// </summary>
+    /// <param name="query">function to apply as a query against the data</param>
+    Task<List<Product>> Query(Func<Product, bool> query);
+
+    /// <summary>
+    /// Updates the quantities for a product.
+    /// </summary>
+    /// <param name="model">The update model.</param>
+    Task UpdateQuantities(Product model);
+
+    /// <summary>
+    /// Inserts a new product
+    /// </summary>
+    /// <param name="model">The model.</param>
+    Task<Product> Insert(Product model);
+}
+```
+## Definition for EPM.Mouser.Interview.Models
+```
+ public class Product
+{
+    /// <summary>
+    /// Product Id
+    /// </summary>
+    public long Id { get; set; }
+
+    /// <summary>
+    /// Product Name
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Quantity currently in the warehouse
+    /// </summary>
+    public int InStockQuantity { get; set; }
+
+    /// <summary>
+    /// Quantity reserved for existing orders
+    /// </summary>
+    public int ReservedQuantity { get; set; }
+}
+
+/// <summary>
+/// Reason for failed requests
+/// </summary>
+public enum ErrorReason
+{
+    QuantityInvalid,
+    NotEnoughQuantity,
+    InvalidRequest
+}
+
+/// <summary>
+/// Standard Response for Update Requests
+/// </summary>
+public class UpdateResponse
+{
+    /// <summary>
+    /// Gets or sets the error reason when Success is false.
+    /// </summary>
+    /// <value>
+    /// The error reason.
+    /// </value>
+    public ErrorReason? ErrorReason { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the update was a success.
+    /// </summary>
+    /// <value>
+    ///   <c>true</c> if success; otherwise, <c>false</c>.
+    /// </value>
+    public bool Success { get; set; }
+}
+
+/// <summary>
+/// Standard response for Create Requests
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <seealso cref="UpdateResponse" />
+public class CreateResponse<T> : UpdateResponse
+{
+    /// <summary>
+    /// Gets or sets the model created.
+    /// </summary>
+    public T Model { get; set; }
+}
+
+public class UpdateQuantityRequest
+{
+    /// <summary>
+    /// Gets or sets the Id of the product to update.
+    /// </summary>
+    public long Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the quantity change being requested.
+    /// </summary>
+    public int Quantity { get; set; }
+}
+```
